c6da14289279cc824ffee180ae6f6a8582e9c4f2
AbeRodz <abrahamrodz17@unitec.edu>
2025-05-11 10:26:36 -0600

    UPDATE: added SSE and stream support, fixed runtime panic when decoding

--- src/api/handlers.zig
+++ src/api/handlers.zig
@@ -3,12 +3,10 @@ const tk = @import("tokamak");
 const ChatCompletionRequest = @import("completions.zig").ChatCompletionRequest;
 const ChatCompletionResponse = @import("completions.zig").ChatCompletionResponse;
 const Choice = @import("completions.zig").Choice;
-const Message = @import("completions.zig").Message;
+const ResponseMessage = @import("completions.zig").ResponseMessage;
 const Content = @import("completions.zig").Content;
-const ContentObject = @import("completions.zig").ContentObject;
-const Stop = @import("completions.zig").Stop;
-const StreamOptions = @import("completions.zig").StreamOptions;
 const llama = @import("../llama/llama.zig");
+const CompletionStreamer = @import("completions_stream.zig").CompletionStreamer;
 const parser = @import("completions_parser.zig");
 const uuid = @import("uuid");
 
@@ -53,8 +51,8 @@ pub fn handleCompletion(ctx: tk.Context, allocator: std.mem.Allocator) !ChatComp
     model_response = try llama.respondToPrompt(allocator, req.model, 8192, prompt.items);
     var choices = try allocator.alloc(Choice, 1);
     choices[0] = .{
-        .message = Message{
-            .content = .{ .plain = model_response },
+        .message = ResponseMessage{
+            .content = model_response,
             .role = "assistant",
         },
         .index = 0,
@@ -77,4 +75,64 @@ pub fn handleCompletion(ctx: tk.Context, allocator: std.mem.Allocator) !ChatComp
             .total_tokens = 2,
         },
     };
+}
+
+pub fn handleCompletionStream(ctx: *tk.Context, allocator: std.mem.Allocator) !void {
+    const body = ctx.req.body().?;
+    const req = try parser.decodeCompletionRequest(allocator, body);
+
+    var prompt = std.ArrayList(u8).init(allocator);
+    defer prompt.deinit();
+
+    for (req.messages) |msg| {
+        const content = parser.getTextContent(msg);
+        if (content) |val| {
+            const prefix = msg.role;
+            try prompt.appendSlice(prefix);
+            try prompt.appendSlice(val);
+            try prompt.appendSlice("\n");
+        }
+    }
+
+    if (req.stream == true) {
+        const impl = try CompletionStreamer.init(allocator, req, prompt.items);
+        const StreamType = tk.EventStream(CompletionStreamer);
+        var stream = StreamType{ .impl = impl };
+        try stream.sendResponse(ctx);
+        return;
+    }
+
+    // non-streaming response
+    const full_response = try llama.respondToPrompt(allocator, req.model, 8192, prompt.items);
+
+    var choices = try allocator.alloc(Choice, 1);
+    choices[0] = .{
+        .message = ResponseMessage{
+            .content = full_response,
+            .role = "assistant",
+        },
+        .index = 0,
+        .finish_reason = "stop",
+    };
+    const uuidUrn = uuid.urn.serialize(uuid.v4.new());
+    const id = try allocator.alloc(u8, uuidUrn.len);
+    @memcpy(id, &uuidUrn);
+
+    const res = ChatCompletionResponse{
+        .id = id,
+        .object = "chat.completion",
+        .model = req.model,
+        .created = std.time.timestamp(),
+        .system_fingerprint = "zLLM",
+        .choices = choices,
+        .usage = .{
+            .prompt_tokens = 1,
+            .completion_tokens = 1,
+            .total_tokens = 2,
+        },
+    };
+    ctx.res.content_type = .JSON;
+    ctx.res.status = 200;
+    try ctx.res.json(res, .{});
+    try ctx.res.write();
 }
